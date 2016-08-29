69c9d785fcd0f84786b2e1d09cbe22ba074b3d57
Chuck_Chen <chenlei1699@163.com>
2016-08-29 16:03:06 +0800

    delete

--- qdong_communal_library/src/main/java/com/qdong/communal/library/imageLoader/URLImageCallback.java
+++
@@ -1,9 +0,0 @@
-package com.qdong.communal.library.imageLoader;
-
-import android.graphics.Bitmap;
-import android.widget.ImageView;
-
-public interface URLImageCallback extends ImageCallback {
-	void imageLoadBefore(Bitmap imageDrawable);
-	void imageLoadComplete(Bitmap imageDrawable, ImageView imageView);
-}
--- qdong_communal_library/src/main/java/com/qdong/communal/library/widget/RefreshRecyclerView/listener/OnLoadMoreListener.java
+++
@@ -1,9 +0,0 @@
-package com.qdong.communal.library.widget.RefreshRecyclerView.listener;
-
-/**
- * Created by Syehunter on 2015/11/21.
- */
-public interface OnLoadMoreListener {
-
-    void onLoadMore();
-}
